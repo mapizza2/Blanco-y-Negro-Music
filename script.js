80d2e7d5fa0384d02ec69ecbddf4d815086c1d04
mjmontero <mjmontero@gmail.com>
2024-04-19 13:30:05 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function showPiano() {
+    const content = document.getElementById('content');
+    content.innerHTML = `
+        <div class="piano">
+            <div class="key white" onclick="keyPressed('Do')"></div>
+            <div class="key black" onclick="keyPressed('Do#')"></div>
+            <div class="key white" onclick="keyPressed('Re')"></div>
+            <div class="key black" onclick="keyPressed('Re#')"></div>
+            <div class="key white" onclick="keyPressed('Mi')"></div>
+            <div class="key white" onclick="keyPressed('Fa')"></div>
+            <div class="key black" onclick="keyPressed('Fa#')"></div>
+            <div class="key white" onclick="keyPressed('Sol')"></div>
+            <div class="key black" onclick="keyPressed('Sol#')"></div>
+            <div class="key white" onclick="keyPressed('La')"></div>
+            <div class="key black" onclick="keyPressed('La#')"></div>
+            <div class="key white" onclick="keyPressed('Si')"></div>
+        </div>
+    `;
+}
+
+function keyPressed(note) {
+    alert('Tecla ' + note + ' pulsada!');
+}
