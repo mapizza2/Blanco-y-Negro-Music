ccd1c73c65afa9521a71c83b0669420d39f8553e
mjmontero <mjmontero@gmail.com>
2024-04-19 13:34:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function showPiano() {
+        const content = document.getElementById('content');
+        content.innerHTML = `
+            <div class="piano">
+                <div class="key white" onclick="keyPressed('Do')">Do</div>
+                <div class="key black" onclick="keyPressed('Do#')">Do#</div>
+                <div class="key white" onclick="keyPressed('Re')">Re</div>
+                <div class="key black" onclick="keyPressed('Re#')">Re#</div>
+                <div class="key white" onclick="keyPressed('Mi')">Mi</div>
+                <div class="key white" onclick="keyPressed('Fa')">Fa</div>
+                <div class="key black" onclick="keyPressed('Fa#')">Fa#</div>
+                <div class="key white" onclick="keyPressed('Sol')">Sol</div>
+                <div class="key black" onclick="keyPressed('Sol#')">Sol#</div>
+                <div class="key white" onclick="keyPressed('La')">La</div>
+                <div class="key black" onclick="keyPressed('La#')">La#</div>
+                <div class="key white" onclick="keyPressed('Si')">Si</div>
+            </div>
+        `;
+    }
 
-function showPiano() {
-    const content = document.getElementById('content');
-    content.innerHTML = `
-        <div class="piano">
-            <div class="key white" onclick="keyPressed('Do')"></div>
-            <div class="key black" onclick="keyPressed('Do#')"></div>
-            <div class="key white" onclick="keyPressed('Re')"></div>
-            <div class="key black" onclick="keyPressed('Re#')"></div>
-            <div class="key white" onclick="keyPressed('Mi')"></div>
-            <div class="key white" onclick="keyPressed('Fa')"></div>
-            <div class="key black" onclick="keyPressed('Fa#')"></div>
-            <div class="key white" onclick="keyPressed('Sol')"></div>
-            <div class="key black" onclick="keyPressed('Sol#')"></div>
-            <div class="key white" onclick="keyPressed('La')"></div>
-            <div class="key black" onclick="keyPressed('La#')"></div>
-            <div class="key white" onclick="keyPressed('Si')"></div>
-        </div>
-    `;
-}
+    function keyPressed(note) {
+        alert('Tecla ' + note + ' pulsada!');
+    }
 
-function keyPressed(note) {
-    alert('Tecla ' + note + ' pulsada!');
-}
+    window.showPiano = showPiano; // Hacer la función accesible globalmente
+});
