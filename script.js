b858f9a64141f29ceef088f61d8dc4531787bdb6
mjmontero <mjmontero@gmail.com>
2024-04-19 13:51:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function showPiano() {
-        const content = document.getElementById('content');
-        content.innerHTML = `
-            <div class="piano">
-                <div class="key white" onclick="keyPressed('Do')">Do</div>
-                <div class="key black" onclick="keyPressed('Do#')">Do#</div>
-                <div class="key white" onclick="keyPressed('Re')">Re</div>
-                <div class="key black" onclick="keyPressed('Re#')">Re#</div>
-                <div class="key white" onclick="keyPressed('Mi')">Mi</div>
-                <div class="key white" onclick="keyPressed('Fa')">Fa</div>
-                <div class="key black" onclick="keyPressed('Fa#')">Fa#</div>
-                <div class="key white" onclick="keyPressed('Sol')">Sol</div>
-                <div class="key black" onclick="keyPressed('Sol#')">Sol#</div>
-                <div class="key white" onclick="keyPressed('La')">La</div>
-                <div class="key black" onclick="keyPressed('La#')">La#</div>
-                <div class="key white" onclick="keyPressed('Si')">Si</div>
-            </div>
-        `;
-    }
+    const pianoLink = document.getElementById('piano-link');
+    const bateriaLink = document.getElementById('bateria-link');
+    const content = document.getElementById('content');
 
-    function keyPressed(note) {
-        alert('Tecla ' + note + ' pulsada!');
-    }
+    pianoLink.addEventListener('click', function() {
+        fetch('piano.html')
+            .then(response => response.text())
+            .then(html => {
+                content.innerHTML = html;
+                // Agrega la hoja de estilo del piano
+                if (!document.getElementById('piano-style')) {
+                    var head = document.head;
+                    var link = document.createElement('link');
+                    link.id = 'piano-style';
+                    link.rel = 'stylesheet';
+                    link.type = 'text/css';
+                    link.href = 'piano.css';
+                    link.media = 'all';
+                    head.appendChild(link);
+                }
+            });
+    });
 
-    window.showPiano = showPiano; // Hacer la función accesible globalmente
+    bateriaLink.addEventListener('click', function() {
+        content.innerHTML = '<p>Mostrando información sobre la Batería.</p>';
+    });
 });
+
+function keyPressed(note) {
+    alert('Tecla ' + note + ' pulsada!');
+}
